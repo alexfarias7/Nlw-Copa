5284dca6d8eed844b4eddf3f62be4f237c72719b
alex farias <alexfarias44@gmail.com>
2022-11-07 17:13:52 -0400

    add class 04 and 05

---
+++ server/src/routes/pools.ts
@@ -0,0 +1,206 @@
+import { FastifyInstance } from "fastify";
+
+import ShortUniqueId from "short-unique-id";
+import { z } from "zod";
+import { prisma } from "../lib/prima";
+import { authenticate } from "../plugins/Authenticate";
+
+export async function poolRoutes(fastify: FastifyInstance) {
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  fastify.get(
+    "/pools",
+    {
+      onRequest: [authenticate],
+    },
+    async (request) => {
+      const pools = await prisma.pool.findMany({
+        where: {
+          Participant: {
+            some: {
+              userId: request.user.sub,
+            },
+          },
+        },
+        include: {
+          _count: {
+            select: {
+              Participant: true,
+            },
+          },
+
+          Participant: {
+            select: {
+              id: true,
+
+              user: {
+                select: {
+                  avatarUrl: true,
+                },
+              },
+            },
+            take: 4,
+          },
+
+          owner: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
+
+      return { pools };
+    }
+  );
+
+  fastify.get(
+    "/pools/:id",
+    {
+      onRequest: [authenticate],
+    },
+    async (request) => {
+      const getPoolParamas = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPoolParamas.parse(request.params);
+
+      const pool = await prisma.pool.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          _count: {
+            select: {
+              Participant: true,
+            },
+          },
+
+          Participant: {
+            select: {
+              id: true,
+
+              user: {
+                select: {
+                  avatarUrl: true,
+                },
+              },
+            },
+            take: 4,
+          },
+
+          owner: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
+
+      return { pool };
+    }
+  );
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({
+      length: 6,
+    });
+
+    const code = String(generate()).toUpperCase();
+
+    try {
+      await request.jwtVerify();
+
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+          ownerId: request.user.sub,
+
+          Participant: {
+            create: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        },
+      });
+    }
+
+    return await reply.status(201).send({ title, code });
+  });
+
+  fastify.post(
+    "/pools/join",
+    {
+      onRequest: [authenticate],
+    },
+    async (request, reply) => {
+      const joinPoolBody = z.object({
+        code: z.string(),
+      });
+
+      const { code } = joinPoolBody.parse(request.body);
+
+      const pool = await prisma.pool.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          Participant: {
+            where: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+
+      if (!pool) {
+        return await reply.status(404).send({ message: "Pool not found." });
+      }
+
+      if (pool.Participant.length > 0) {
+        return await reply.status(409).send({ message: "Already joined." });
+      }
+
+      if (!pool.ownerId) {
+        await prisma.pool.update({
+          where: {
+            id: pool.id,
+          },
+          data: {
+            ownerId: request.user.sub,
+          },
+        });
+      }
+
+      await prisma.participant.create({
+        data: {
+          poolId: pool.id,
+          userId: request.user.sub,
+        },
+      });
+
+      return await reply.status(201).send();
+    }
+  );
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
-import ShortUniqueId from "short-unique-id";
-import { z } from "zod";
+import jwt from "@fastify/jwt";
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+import { poolRoutes } from "./routes/pools";
+import { authRoutes } from "./routes/autgh";
+import { gameRoutes } from "./routes/game";
+import { guessRoutes } from "./routes/guess";
+import { userRoutes } from "./routes/user";
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -17,42 +17,17 @@ async function bootstrap() {
     origin: true,
   });
 
-  fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count();
-
-    return { count };
-  });
-  fastify.get("/users/count", async () => {
-    const count = await prisma.user.count();
-
-    return { count };
-  });
-  fastify.get("/guesses/count", async () => {
-    const count = await prisma.guess.count();
-
-    return { count };
+  await fastify.register(jwt, {
+    secret: "nlwcopa",
   });
 
-  fastify.post("/pools", async (request, reply) => {
-    const createPoolBody = z.object({
-      title: z.string(),
-    });
-    const { title } = createPoolBody.parse(request.body);
+  await fastify.register(authRoutes);
+  await fastify.register(gameRoutes);
+  await fastify.register(guessRoutes);
+  await fastify.register(poolRoutes);
+  await fastify.register(userRoutes);
 
-    const generate = new ShortUniqueId({ length: 6 });
-    const code = String(generate()).toUpperCase();
-
-    await prisma.pool.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    return reply.status(201).send({ code });
-  });
-
-  await fastify.listen({ port: 3333 /* host: "0.0.0.0" */ });
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();
